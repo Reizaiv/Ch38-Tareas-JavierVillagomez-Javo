d8696ed48f32147617f934370cc19a29d31cf78c
Javier Villagomez <javier_diaz16@hotmail.com>
2024-03-02 23:41:04 -0600

    ci: se completo el ejercicio en funcionalidad de js, se pregunta nombre y cambia en html

---
+++ assets/js/deployMenu.js
@@ -0,0 +1,17 @@
+function insertPrincipalMenu() {
+
+    const menuRef = document.getElementById("menu");
+
+    menuRef.innerHTML = `
+    <nav>
+        <ul>
+            <li><a href="./pages/web-07-Aplicaciones-Web-Ejercicio-1.html">Tareas Tan</a></li>
+            <li><a href="">Tareas Serch</a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+    `  
+}
+
+
+insertPrincipalMenu();
---
+++ assets/js/welcomeMessage.js
@@ -0,0 +1,21 @@
+/*
+function welcomeMessage() {
+    //const messageRef = document.getElementById("initial-message");
+    document.getElementById("initial-message").innerHTML = `<h1> Hola, <em id = "name-visitor">visitante</em>, bienvenido</h1>`;
+}*/
+
+function getName() {
+    
+}
+
+function changeWelcomeName() {
+    const name = getName()
+    console.log(typeof(name));
+    document.getElementById("name-visitor").innerHTML = `${name}`;
+}
+
+function getName() {
+    const nombre = prompt(`¿Cuál es tu nombre?`);    
+}
+
+welcomeMessage();
